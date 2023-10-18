1f1cc9ad75b81662e2e6b9f1ec9d8096f8842227
Adei Laka <adei.laka@opendeusto.es>
2023-10-18 09:39:45 +0200

    pruebas basicas

--- Prueba/src/tema_4/PruebaListas.java
+++ Prueba/src/tema_4/PruebaListas.java
@@ -1,8 +1,40 @@
 package tema_4;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class PruebaListas {
 
 	public static void main(String[] args) {
+		
+		List<Integer> listaEnt = new ArrayList<>();
+		
+		listaEnt.add(-4);
+		listaEnt.add(6);
+		listaEnt.add(-9);
+		listaEnt.add(1);
+		
+		System.out.println(listaEnt.size()+"\n");
+		
+		System.out.println(listaEnt.isEmpty()+"\n");
+		
+		for (Integer i : listaEnt) {
+			System.out.println("/////"+i);
+		}
+		
+		//Ordenar lista de Integer
+		
+		Collections.sort(listaEnt);
+		
+		int indice = 0;
+		while(indice < listaEnt.size()) {
+			System.out.println(listaEnt.get(indice));
+			indice++;
+		}
+		
+		
+		
 		
 		
 	}
